c9b672027fab78ff25ae0b4ed46131e49336cd5f
abrahamyan222 <116426574+abrahamyan222@users.noreply.github.com>
2026-05-13 04:19:11 +0400

    Update GameUtils.java

--- src/Utils/GameUtils.java
+++ src/Utils/GameUtils.java
@@ -2,7 +2,17 @@ package Utils;
 
 import Core.Team;
 
+/**
+ * Utility methods for the game.
+ */
 public class GameUtils {
+
+    /**
+     * Returns the display name for a given team.
+     *
+     * @param team the team to look up
+     * @return "Defenders" for {@link Team#PLAYER}, "Invaders" for {@link Team#ENEMY}
+     */
     public static String getTeamName(Team team) {
         if (team == Team.PLAYER) {
             return "Defenders";
